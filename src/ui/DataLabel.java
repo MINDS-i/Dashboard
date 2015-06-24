4e7b29944d8bbd882a958c244915ec959165df11
Brett Menzies <scimor5@gmail.com>
2015-06-24 10:23:38 -0700

    Stopped negative sign boggling

--- src/ui/DataLabel.java
+++ src/ui/DataLabel.java
@@ -11,39 +11,43 @@ public class DataLabel extends JLabel implements TelemetryListener{
   private String label;
   private String suffix;
   private double data;
+  private void updateText(){
+    String dataString = ((data >= 0)? " " : "") + String.format("%f", data);
+    setText(label + dataString + suffix);
+  }
   public DataLabel(String prefix, double dat, String units){
     label = prefix;
     data = dat;
     suffix = units;
-    setText(label + String.format("%f", data)  + suffix);
+    updateText();
   }
 
   public DataLabel(String prefix, String units){
     label = prefix;
     data = 0.;
     suffix = units;
-    setText(label + String.format("%f", data) + suffix);
+    updateText();
   }
 
   public DataLabel(String prefix){
     label = prefix;
     data = 0.;
     suffix = new String();
-    setText(label + String.format("%f", data) + suffix);
+    updateText();
   }
   public void update(double dat){
     data = dat;
-    setText(label + String.format("%f", data) + suffix);
+    updateText();
   }
 
   public void setLabel(String prefix){
     label = prefix;
-    setText(label + String.format("%f", data) + suffix);
+    updateText();
   }
 
   public void setUnits(String units){
     suffix = units;
-    setText(label + String.format("%f", data) + suffix);
+    updateText();
   }
 
   public double getData(){
