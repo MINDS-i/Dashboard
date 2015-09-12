e593d7faa63d82c47f1461072bafb917ed447e44
Brett Menzies <scimor5@gmail.com>
2015-09-12 14:12:51 -0700

    Improved map panel license documentation

--- src/map/MapPanel.java
+++ src/map/MapPanel.java
@@ -1,12 +1,11 @@
 /*******************************************************************************
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * The source code for this class
+ * is made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Stepan Rutz - initial implementation
- *    Brett Menzies - MINDS-i adaptation
+ * This code is based on the MapPanel source code written by Stepan Rutz
+ * found at: http://mappanel.sourceforge.net/
  ******************************************************************************/
 
 package com.map;
