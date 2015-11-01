bb753a417b15e909bd2132f96b0f31eecd6ec9d3
Brett Menzies <scimor5@gmail.com>
2015-11-01 12:49:29 -0800

    First pass at uncoupled serial decoder

---
+++ test/Decoder_test.java
@@ -0,0 +1,11 @@
+import com.serial.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Decoder_test {
+    @Test
+    public void testStartup() {
+        Decoder decoder = new Decoder(null,null,null,null);
+    }
+}
