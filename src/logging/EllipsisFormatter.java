cd38f362584390f12b0ebcbe538401198b9615d9
Brett Menzies <scimor5@gmail.com>
2016-05-26 16:57:07 -0700

    EllipsisFormatter implemented

--- src/logging/EllipsisFormatter.java
+++ src/logging/EllipsisFormatter.java
@@ -3,23 +3,31 @@ package com.logging;
 import java.util.logging.*;
 
 public class EllipsisFormatter extends Formatter{
+    private interface Content{ String get(LogRecord l); }
+    private final Content content;
+    private final int maxLineLength;
     /**
      * A formatter that ellipsizes each logRecord's raw message at maxLineLength
      */
     public EllipsisFormatter(int maxLineLength){
-
+        content = (LogRecord l) -> l.getMessage();
+        this.maxLineLength = maxLineLength;
     }
     /**
      * Make a formatter that ellipsizes the output of parent at maxLineLength
      */
     public EllipsisFormatter(Formatter parent, int maxLineLength){
-
+        content = (LogRecord l) -> parent.format(l);
+        this.maxLineLength = maxLineLength;
     }
     /** Fromat the given log record and return the formatted string */
     public String format(LogRecord lr){
-        return "";
+        return ellipsize(content.get(lr));
     }
+    /** Make a string one line of at most maxLineLength chars */
     private String ellipsize(String s){
-        return "";
+        String line = (s.length() <= maxLineLength)? s
+                        : s.substring(0,maxLineLength-3) + "...";
+        return line.replace("\n"," ");
     }
 }
--- test/com/logging/Ellipsisformatter_test.java
+++ test/com/logging/Ellipsisformatter_test.java
@@ -5,36 +5,42 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
 
-import java.util.logger.*;
+import java.util.logging.*;
 
 public class Ellipsisformatter_test{
+    final Level l = Level.ALL;
     @Test
     public void testNoEllipsisNecessary(){
-        assertThat();
+        EllipsisFormatter ef = new EllipsisFormatter(10);
+        assertThat(ef.format(new LogRecord(l,"abcd")), equalTo("abcd"));
     }
-
     @Test
     public void testNoEllipsisMaxLength(){
-
+        EllipsisFormatter ef = new EllipsisFormatter(4);
+        assertThat(ef.format(new LogRecord(l,"abcd")), equalTo("abcd"));
     }
-
     @Test
     public void testEllipsisOneTooLong(){
-
+        EllipsisFormatter ef = new EllipsisFormatter(4);
+        assertThat(ef.format(new LogRecord(l,"abcde")), equalTo("a..."));
     }
-
     @Test
     public void testEllipsisVeryLong(){
-
+        EllipsisFormatter ef = new EllipsisFormatter(4);
+        assertThat(ef.format(new LogRecord(l,"abcdefghijkl")), equalTo("a..."));
     }
-
     @Test
     public void testFormatterPassthrough(){
-
+        Formatter f = mock(Formatter.class);
+        when(f.format(org.mockito.Mockito.any(LogRecord.class)))
+            .thenReturn("abcd");
+        EllipsisFormatter ef = new EllipsisFormatter(f, 4);
+        assertThat(ef.format(new LogRecord(l,"nope")), equalTo("abcd"));
     }
-
     @Test
-    public void testLambdaFormatting(){
-
+    public void testScrubNewlines(){
+        EllipsisFormatter ef = new EllipsisFormatter(10);
+        assertThat(ef.format(new LogRecord(l,"abc\ndef")),
+                                     equalTo("abc def"));
     }
 }
