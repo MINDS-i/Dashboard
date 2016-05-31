c84c5875b2f6f7f3fc1cddf1d78d6043ac5cbc45
Brett Menzies <scimor5@gmail.com>
2016-05-31 10:12:45 -0700

    Update TableColumn interface documentation

--- src/table/TableColumn.java
+++ src/table/TableColumn.java
@@ -1,10 +1,16 @@
 package com.table;
 
 public interface TableColumn {
+    /** Returns the name of this Column */
     public String getName();
+    /** Returns the value for the coll on `row` */
     public Object getValueAt(int row);
+    /** Returns the number of rows in this column */
     public int getRowCount();
-    public Class getDataClass();
+    /** Returns the most specific superclass of all cell values */
+    public Class<?> getDataClass();
+    /** Returns true if the cell on `row` is editable */
     public boolean isRowEditable(int row);
+    /** Sets the value in the coll `row` to `value` */
     public void setValueAt(Object value, int row);
 }
