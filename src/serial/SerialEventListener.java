69df833a256ea684caff4fcf082aa5b44afaa213
Brett Menzies <scimor5@gmail.com>
2016-05-31 09:36:26 -0700

    Document serialEventListener methods

--- src/serial/SerialEventListener.java
+++ src/serial/SerialEventListener.java
@@ -3,6 +3,17 @@ package com.serial;
 import jssc.SerialPort;
 
 public interface SerialEventListener {
+    /**
+     * Fired when a successful connection is established
+     * @param newConnection The new SerialPort object
+     */
     public void connectionEstablished(SerialPort newConnection);
+    /**
+     * Fired when the user makes a request to disconnect the serial port
+     * The SerialPort object will be closed when all disconnect Requests
+     *     have finished
+     * The handler should be prepared for future connections to be made after
+     *     a disconnect
+     */
     public void disconnectRequest();
 }
