06171bdb71e769751609fa394c3711da56d10a21
Brett Menzies <scimor5@gmail.com>
2016-10-21 09:51:49 -0700

    Barebones artificial horizon, not correctly scaled angularly

---
+++ src/ui/ArtificialHorizon.java
@@ -0,0 +1,146 @@
+package com.ui;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.awt.FontMetrics;
+import java.awt.image.BufferedImage;
+import java.util.Collection;
+import java.util.concurrent.*;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.List;
+
+//for testing purposes
+import java.io.File;
+
+public class ArtificialHorizon extends JPanel {
+
+    private double pitch, roll;
+    private Color groundColor = new Color(0x7D5233);
+    private Color airColor = new Color(0x5B93C5);
+    private Color wingColor = new Color(0xEA8300);
+    private Color barColor = Color.WHITE;
+
+    public ArtificialHorizon(){
+        ScheduledExecutorService ex =
+            Executors.newSingleThreadScheduledExecutor();
+        ex.scheduleAtFixedRate(
+            ()->{ this.repaint(); Toolkit.getDefaultToolkit().sync(); },
+            0,
+            20,
+            TimeUnit.MILLISECONDS);
+    }
+
+    public void set(double pitch, double roll){
+        synchronized(this){
+            this.pitch = pitch;
+            this.roll = roll;
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int size = Math.min(getHeight(), getWidth());
+        Graphics2D g2d = (Graphics2D)g.create();
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHints(rh);
+        render(g2d, size);
+        g2d.dispose();
+
+        g.setColor(Color.BLACK);
+        int box = Math.max(getHeight(), getWidth());
+        g.fillRect(0, size, box, box);
+        g.fillRect(size, 0, box, box);
+    }
+
+    private void render(Graphics2D g, int size){
+        Graphics2D background = (Graphics2D)g.create();
+        background.translate(size/2, size/2);
+        background.rotate(roll);
+        background.translate(-size, -size);
+        renderBackground(background, size*2);
+        background.dispose();
+
+        paintPlaneIndicator(g, size);
+    }
+
+    private void paintPlaneIndicator(Graphics2D g, int size){
+        int height = size/40;
+        int width = size*6/40;
+        int seperation = width;
+
+        g.setColor(wingColor);
+        g.fillRoundRect(size/2 - width/2 - seperation, size/2 - height/2,
+                        width, height,
+                        height, height);
+
+        g.fillOval(size/2 - height/2, size/2 - height/2, height, height);
+
+        g.fillRoundRect(size/2 - width/2 + seperation, size/2 - height/2,
+                        width, height,
+                        height, height);
+
+    }
+
+    int angleOffset(double theta, int size){
+        return (int)((theta/2.0)*(double)size)+size/2;
+    }
+
+    private void renderBackground(Graphics2D g, int size){
+        synchronized(this){
+            int horizonPosition = angleOffset(pitch, size);
+
+            g.setColor(airColor);
+            g.fillRect(0, 0, size, horizonPosition);
+
+            g.setColor(groundColor);
+            g.fillRect(0, horizonPosition, size, size-horizonPosition);
+
+            g.setColor(Color.BLACK);
+            g.fillRect(0, horizonPosition-1, size, 2);
+
+            g.setColor(barColor);
+            int lineHeight = Math.max(size/200, 1);
+            int lineWidth = size*30/200;
+            for(int i=-60; i<=60; i+=10){
+                if(i == 0) continue;
+                int position = angleOffset(((double)i)*Math.PI/180.0+pitch, size);
+                g.fillRoundRect(
+                    size/2-lineWidth/2, position-lineHeight/2,
+                    lineWidth, lineHeight,
+                    lineHeight, lineHeight);
+            }
+        }
+
+    }
+
+    //main method for quicker manual testing
+    public static void main(String[] args) {
+        ArtificialHorizon ah = new ArtificialHorizon();
+
+        JFrame f = new JFrame("ArtificialHorizon");
+        f.add(ah);
+        f.pack();
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        double wave = 0.0;
+        while(f.isShowing()) {
+            try {
+                wave += 0.01;
+                ah.set(Math.cos(wave)/3.0,Math.sin(wave*(3.0/5.0))/10.0);
+                Thread.sleep(20);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
