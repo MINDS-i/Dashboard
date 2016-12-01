d5f4fe12e7bc10312ee7910da819631b32784db7
Brett Menzies <scimor5@gmail.com>
2016-11-30 18:46:05 -0800

    More work towards radio bar widget

--- src/ui/RadioWidget.java
+++ src/ui/RadioWidget.java
@@ -19,9 +19,20 @@ import javax.xml.stream.*;
 import java.text.ParseException;
 
 public class RadioWidget{
+    /**
+     * Create a square radio widget displaying the five radio telemetry
+     *   lines as bar graphs in a `size`^2 region
+     */
     public static JPanel create(Context ctx, int size){
         JPanel container = new TransparentPanel(ctx, size);
 
+        JPanel widget = new Bars();
+        widget.setPreferredSize(new Dimension(size,size));
+
         return container;
+    }
+
+    private static class Bars extends JPanel{
+        Bars(){}
     }
 }
