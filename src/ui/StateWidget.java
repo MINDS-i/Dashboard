e0a4ff3b7d178820285590b6bc07054234810b47
Chris Park <68874725+ChrisParkInfinetix@users.noreply.github.com>
2020-11-06 10:38:18 -0800

    Added coloring to state displays
    Current colors cycle between Red/Yellow/Green based on severity of the state

--- src/ui/StateWidget.java
+++ src/ui/StateWidget.java
@@ -16,15 +16,21 @@ import com.ui.ninePatch.NinePatchPanel;
  * described over serial communication.
  */
 public class StateWidget extends NinePatchPanel {
+	//Constants
+	protected static final int BORDER_SIZE = 25;
+	
 	private Context context;
 	private JFrame infoFrame;
 	
-	private JLabel apmLabel;
+	//State readouts
+	private JPanel apmPanel;
+	private JLabel apmLabel;	
+	private JPanel drivePanel;
 	private JLabel driveLabel;
+	private JPanel autoPanel;
 	private JLabel autoLabel;
+	private JPanel flagPanel;
 	private JLabel flagLabel;
-	
-	protected static final int BORDER_SIZE = 25;
 	
 	/**
 	 * Class constructor
@@ -40,39 +46,60 @@ public class StateWidget extends NinePatchPanel {
 	 * Construct and place the visual elements of the widget
 	 */
 	private void initPanel() {
-//		Theme theme 		= context.theme;
 		Dimension spacer 	= new Dimension(0, 5);
 		Dimension labelSize = new Dimension(90, 25);
-		
-		apmLabel 	= new JLabel("APM - ??");
-		driveLabel 	= new JLabel("DRV - ??");
-		autoLabel 	= new JLabel("AUT - ??");
-		flagLabel 	= new JLabel("FLG - None");
-		
-		
-		ArrayList<JPanel> statePanels = new ArrayList<JPanel>();
-		JComponent[] formatList = new JComponent[] {
-			apmLabel, driveLabel, autoLabel, flagLabel	
-		};
-		
-		for(JComponent jc : formatList) {
-			jc.setAlignmentX(Component.CENTER_ALIGNMENT);
-			jc.setMaximumSize(labelSize);
-			
-			JPanel panel = new JPanel();
-			panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
-			panel.setPreferredSize(labelSize);
-			panel.setOpaque(false);
-			panel.add(jc);
-			
-			statePanels.add(panel);
-			
-		}
+		Dimension panelSize = new Dimension(90, 25);
 		
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		this.setBorder(BorderFactory.createEmptyBorder(
 				BORDER_SIZE, BORDER_SIZE,BORDER_SIZE, BORDER_SIZE));
 
+		//Configure state labels
+		apmLabel 	= new JLabel("APM - ??");
+		driveLabel 	= new JLabel("DRV - ??");
+		autoLabel 	= new JLabel("AUT - ??");
+		flagLabel 	= new JLabel("FLG - None");
+		
+		JComponent[] labelList = new JComponent[] {
+			apmLabel, driveLabel, autoLabel, flagLabel	
+		};
+		
+		for(JComponent jc : labelList) {
+			jc.setAlignmentX(Component.CENTER_ALIGNMENT);
+			jc.setMaximumSize(labelSize);
+		}
+		
+		//Configure state panels
+		ArrayList<JPanel> statePanels = new ArrayList<JPanel>();
+		apmPanel = new JPanel();
+		apmPanel.setLayout(new BoxLayout(apmPanel, BoxLayout.LINE_AXIS));
+		apmPanel.setPreferredSize(panelSize);
+		apmPanel.setOpaque(true);
+		apmPanel.add(apmLabel);
+		statePanels.add(apmPanel);
+		
+		drivePanel = new JPanel();
+		drivePanel.setLayout(new BoxLayout(drivePanel, BoxLayout.LINE_AXIS));
+		drivePanel.setPreferredSize(panelSize);
+		drivePanel.setOpaque(true);
+		drivePanel.add(driveLabel);
+		statePanels.add(drivePanel);
+		
+		autoPanel = new JPanel();
+		autoPanel.setLayout(new BoxLayout(autoPanel, BoxLayout.LINE_AXIS));
+		autoPanel.setPreferredSize(panelSize);
+		autoPanel.setOpaque(true);
+		autoPanel.add(autoLabel);
+		statePanels.add(autoPanel);
+		
+		flagPanel = new JPanel();
+		flagPanel.setLayout(new BoxLayout(flagPanel, BoxLayout.LINE_AXIS));
+		flagPanel.setPreferredSize(panelSize);
+		flagPanel.setOpaque(true);
+		flagPanel.add(flagLabel);
+		statePanels.add(flagPanel);
+		
+		//Add panels to widget
 		for(JPanel panel : statePanels) {
 			this.add(panel);
 			this.add(Box.createRigidArea(spacer));
@@ -114,16 +141,19 @@ public class StateWidget extends NinePatchPanel {
 		switch(substate) {
 			case Serial.APM_STATE_INIT:
 				apmLabel.setText("APM - Init");
+				apmPanel.setBackground(Color.white);
 				break;
 			case Serial.APM_STATE_SELF_TEST:
 				apmLabel.setText("APM - Self Test");
+				apmPanel.setBackground(Color.white);
 				break;
 			case Serial.APM_STATE_DRIVE:
 				apmLabel.setText("APM - Driving");
+				apmPanel.setBackground(Color.green);
 				break;
 			default:
 				apmLabel.setText("APM - Unknown");
-				System.err.println("State value: " + substate);
+				apmPanel.setBackground(Color.red);
 		}
 	}
 	
@@ -137,15 +167,19 @@ public class StateWidget extends NinePatchPanel {
 		switch(substate) {
 			case Serial.DRIVE_STATE_STOP:
 				driveLabel.setText("DRV - Stopped");
+				drivePanel.setBackground(Color.white);
 				break;
 			case Serial.DRIVE_STATE_AUTO:
 				driveLabel.setText("DRV - Auto");
+				drivePanel.setBackground(Color.green);
 				break;
 			case Serial.DRIVE_STATE_RADIO:
 				driveLabel.setText("DRV - Manual");
+				drivePanel.setBackground(Color.green);
 				break;
 			default:
 				driveLabel.setText("DRV - Unknown");
+				drivePanel.setBackground(Color.red);
 		}
 	}
 	
@@ -159,15 +193,19 @@ public class StateWidget extends NinePatchPanel {
 		switch(substate) {
 			case Serial.AUTO_STATE_FULL:
 				autoLabel.setText("AUT - Full");
+				autoPanel.setBackground(Color.green);
 				break;
 			case Serial.AUTO_STATE_AVOID:
 				autoLabel.setText("AUT - Avoid");
+				autoPanel.setBackground(Color.yellow);
 				break;
 			case Serial.AUTO_STATE_STALLED:
 				autoLabel.setText("AUT - Stalled");
+				autoPanel.setBackground(Color.red);
 				break;
 			default:
 				autoLabel.setText("AUT - Unknown State");
+				autoPanel.setBackground(Color.red);
 		}
 	}
 	
@@ -185,18 +223,22 @@ public class StateWidget extends NinePatchPanel {
 		
 		if(caution && approach) {
 			flagLabel.setText("FLG - App. & Caut.");
+			flagPanel.setBackground(Color.yellow);
 			//Severity High. Approaching a clear obstable? 
 		}
 		else if(approach) {
 			flagLabel.setText("FLG - Approach");
+			flagPanel.setBackground(Color.yellow);
 			//Severity Medium. Approaching an obstacle, slowing down?
 		}
 		else if(caution) {
 			flagLabel.setText("FLG - Caution");
+			flagPanel.setBackground(Color.yellow);
 			//Severity Medium. Avoiding an obstacle? 
 		}
 		else {
 			flagLabel.setText("FLG - None");
+			flagPanel.setBackground(Color.green);
 			//Severity Low. No hazards detected?
 		}
 	}
