a838338518ff2bca1678326e2a3d6915d81f426e
Chris Park <68874725+ChrisParkInfinetix@users.noreply.github.com>
2021-04-28 15:26:08 -0700

    Update default serial baud rate to 57600
    This is still configurable via the available dropdown list

--- src/serial/Serial.java
+++ src/serial/Serial.java
@@ -97,7 +97,8 @@ public class Serial {
     public static final int	MAX_WAYPOINTS		= 64;
     public static final int	MAX_SETTINGS		= 64;
     public static final int	MAX_TELEMETRY		= 256;
-    public static final int	BAUD				= SerialPort.BAUDRATE_9600;
+//    public static final int	BAUD				= SerialPort.BAUDRATE_9600;
+    public static final int	BAUD				= SerialPort.BAUDRATE_57600;
     public static final int	U16_FIXED_POINT		= 256;
 
     public static final int MAX_CONFIRM_WAIT	= 2000; //in milliseconds
