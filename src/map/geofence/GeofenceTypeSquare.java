b418ad0fb7d76162d104370db3c39b8c693d593d
Chris Park <68874725+ChrisParkInfinetix@users.noreply.github.com>
2022-12-20 14:11:55 -0800

    Add fence draw corrections (#27)
    Added a function to re-calculate the drawn fence diameter based upon pre defined latitude zone ranges. This helps to correct improper fence sizing due to longitudinal distortions that are a side effect of Mercator map projections.

--- src/map/geofence/GeofenceTypeSquare.java
+++ src/map/geofence/GeofenceTypeSquare.java
@@ -114,4 +114,19 @@ public class GeofenceTypeSquare extends GeofenceType {
 		radiusPoint = new Dot(origin.getLatitude(), 
 				origin.getLongitude() + degreesLng, origin.getAltitude());
 	}
+	
+	/**
+	 * Applies a multiplier to the current geofence size to correct
+	 * for drawing size error created by converting Lat/Lng (Spherical)
+	 * coordinate values to 2D (Cartesian) space. This helps the fence
+	 * as drawn to better line up with the real spherical coordinate
+	 * range. The multiplier is determined by pre-defined zones calculated 
+	 * along ranges in latitude.
+	 * @param fenceDiameter - The diameter of the fence to be corrected
+	 * @return - double - the new fence diameter
+	 */
+	protected double applyDrawOffset(double fenceDiameter) {
+		//To Be Implemented
+		return 0.0;
+	}
 }
